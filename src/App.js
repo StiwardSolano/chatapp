fa2526b029fef590bb930318fefe4609fd18d282
Stiward_Solano <13769238+StiwardSolano@users.noreply.github.com>
2021-02-20 17:35:10 -0500

    Login fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import {ChatEngine} from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '0c6642af-f190-4c7c-a408-35dad4592d3b';
+
 const App = () => {
-    
-    if(!localStorage.getItem('username')) return <LoginForm />
-    
-    return (
-        <ChatEngine
-            height="100vh"
-            projectID="0c6642af-f190-4c7c-a408-35dad4592d3b"
-            userName={localStorage.getItem('username')}
-            userSecret={localStorage.getItem('password')}
-            renderChatFeed={(chatAppProps) =><ChatFeed {... chatAppProps} />}
-            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-        />
-    );
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
 
 export default App;
